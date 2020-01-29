9b4e62fd1e17ff1e72f2d6a46669ebc998614206
marchesem@moravian.edu <marchesem@moravian.edu>
2020-01-29 14:27:51 -0500

    Stuff

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Kosack name = new Kosack();
         Valeria name2 = new Valeria();
+        Michael name3 = new Michael("Michael Marchese");
     }
 }
---
+++ Michael.java
@@ -0,0 +1,7 @@
+public class Michael {
+    public String name;
+    public Michael(String name) {
+        this.name = name;
+        System.out.println("Michael");
+    }
+}
